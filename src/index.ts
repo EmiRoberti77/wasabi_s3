31b9f5556cae609f28969e58c24d276fbe1ac637
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-08-01 13:49:49 +0100

    created a .env to store keys and bucket info

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ dotenv.config();
 const REGION: string = 'eu-west-1';
 const a_key = process.env.ACCESS_KEY || '';
 const s_key = process.env.SECRET_ACCESS_KEY || '';
+const bucketName = process.env.BUCKET_NAME || '';
+const filename = process.env.FILENAME || '';
+const body = process.env.BODY || '';
+const success = process.env.SUCCESS || '';
 
 console.log(a_key, s_key, REGION);
 
@@ -19,15 +23,15 @@ const s3Client = new S3Client({
 });
 
 const uploadParam = {
-  Bucket: 'sample-emi',
-  Key: 'hello2.txt',
-  Body: 'hello emi',
+  Bucket: bucketName,
+  Key: filename,
+  Body: body,
 };
 
 const transfer = async () => {
   try {
     const data = await s3Client.send(new PutObjectCommand(uploadParam));
-    console.log('data uploaded');
+    console.log(success);
   } catch (err) {
     console.log(err);
   }
