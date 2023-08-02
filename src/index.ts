3fb091d299958d904d5340a0178c8c1a91a08a64
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-08-02 08:07:49 +0100

    added method to read content as text from S3 bucket

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
+import { Readable } from 'stream';
 
 dotenv.config();
 
 const REGION: string = 'eu-west-1';
+const EMPTY = '';
 const a_key = process.env.ACCESS_KEY || '';
 const s_key = process.env.SECRET_ACCESS_KEY || '';
 const bucketName = process.env.BUCKET_NAME || '';
@@ -22,13 +28,13 @@ const s3Client = new S3Client({
   endpoint: `https://s3.${REGION}.wasabisys.com`,
 });
 
-const uploadParam = {
-  Bucket: bucketName,
-  Key: filename,
-  Body: body,
-};
-
 const transfer = async () => {
+  const uploadParam = {
+    Bucket: bucketName,
+    Key: filename,
+    Body: body,
+  };
+
   try {
     const data = await s3Client.send(new PutObjectCommand(uploadParam));
     console.log(success);
@@ -37,4 +43,24 @@ const transfer = async () => {
   }
 };
 
+const readFile = async () => {
+  const readParam = {
+    Bucket: bucketName,
+    Key: filename,
+  };
+  try {
+    const data = await s3Client.send(new GetObjectCommand(readParam));
+    if (data.Body) {
+      let result = '';
+      for await (const chunk of data.Body as Readable) {
+        result += new TextDecoder().decode(chunk);
+      }
+      console.log(result); // Here is the file content
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 transfer();
+readFile();
